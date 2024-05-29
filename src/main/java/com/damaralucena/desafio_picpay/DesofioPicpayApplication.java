56757e32b3f78ea353b1108988bd3315579e670e
damaralucena <damara.ti.dev@gmail.com>
2024-05-28 21:29:22 -0300

    Initial commit

---
+++ src/main/java/com/damaralucena/desafio_picpay/DesofioPicpayApplication.java
@@ -0,0 +1,13 @@
+package com.damaralucena.desofio_picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesofioPicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesofioPicpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damaralucena/desofio_picpay/DesofioPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damaralucena.desofio_picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesofioPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
